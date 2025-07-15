15f09c5d52080dd9e85a5e29465183e50d10e835
xiaocheengggg <1360778617@qq.com>
2025-07-15 23:17:01 +0800

    【第三次提交】
    11111

--- vs_proj1/vs_proj1.cpp
+++ vs_proj1/vs_proj1.cpp
@@ -1,4 +1,6 @@
-﻿// vs_proj1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿
+/*
+// vs_proj1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
@@ -13,5 +15,26 @@ int main()
 
 	//funct1();
 }
+*/
 
 
+
+
+#include <stdio.h>
+
+#define LENGTH 10
+#define WIDTH  5
+#define NEWLINE '\n'
+
+int main()
+{
+
+	int area;
+
+	area = LENGTH * WIDTH;
+	printf("value of area : %d", area);
+	printf("%c", NEWLINE);
+	printf("%c", NEWLINE);
+
+	return 0;
+}
